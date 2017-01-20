b1c87333571dbcaac16410d9eb8563b54e4e3ae8
KevinGilpin <kevin.gilpin@gmail.com>
2017-01-19 22:49:25 -0800

    Replaced ejs with handlebars

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 var express = require('express');
 var app = express();
-var cool = require('cool-ascii-faces');
 var pg = require('pg');
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+var handlebars = require('express-handlebars')
+    .create({ defaultLayout: 'main' });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  response.render('home');
 });
 
 app.get('/cool', function(request, response) {
@@ -43,5 +43,4 @@ app.get('/db', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
 
